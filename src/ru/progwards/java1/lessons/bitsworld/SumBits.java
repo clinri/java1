48ff38a9e359521af1ffe770b748cb307f710fcd
clinri <63862365+clinri@users.noreply.github.com>
2020-05-25 15:43:28 +0300

    HomeWorks H7 fix2

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -1,21 +1,26 @@
 package ru.progwards.java1.lessons.bitsworld;
 
-//1.1 Реализовать функцию public static int sumBits(byte value), которая суммирует все биты параметра value. Например, для двоичного числа 0100101 функция должна вернуть 3.
+//1.1 Реализовать функцию public static int sumBits(byte value), которая суммирует все биты параметра value.
+// Например, для двоичного числа 0100101 функция должна вернуть 3.
 //
 //Подсказки:
 //используйте & с числом 1 для того, чтобы оставить только один правый значащий бит;
 //используйте сдвиг вправо для того, чтобы проверить следующий бит.
 public class SumBits {
-    public static int sumBits(byte value){
-        int sum=0;
-        while (value != 0) {
-            sum+=value&1;
-            value>>>=1;
+    public static int sumBits(byte value) {
+        int sum = 0;
+        int intVal = value & 0b00000000_00000000_00000000_11111111;
+        //System.out.println(Integer.toBinaryString(intVal));
+        while (intVal != 0) {
+            //System.out.println(Integer.toBinaryString(intVal));
+            sum += intVal & 0b00000000_00000000_00000000_00000001;
+            //System.out.println(sum);
+            intVal >>>= 1;
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumBits((byte) 0b0100101));
+        System.out.println(sumBits((byte) 243));
     }
 }
