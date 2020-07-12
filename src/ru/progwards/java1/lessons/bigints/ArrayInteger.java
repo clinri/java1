174b15022634a374b0eacf25a2e3c74bbcd732dc
clinri <63862365+clinri@users.noreply.github.com>
2020-07-12 15:25:59 +0300

    HomeWorks H9 v8 Intensive SeaBattle.Alg v5

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -25,18 +25,19 @@ public class ArrayInteger {
             System.out.println(digits[i]);
         }
     }
-    BigInteger toInt(){
+
+    BigInteger toInt() {
         BigInteger result = new BigInteger("0");
         System.out.println(Arrays.toString(digits));
-        for (int i=0; i<digits.length; i++){
-            result=result.add(BigInteger.TEN.pow(i).multiply(new BigInteger(String.valueOf(digits[i]))));
+        for (int i = 0; i < digits.length; i++) {
+            result = result.add(BigInteger.TEN.pow(i).multiply(new BigInteger(String.valueOf(digits[i]))));
             System.out.println(result);
         }
         System.out.println(result);
         return result;
     }
 
-//    boolean add(ArrayInteger num) {
+    //    boolean add(ArrayInteger num) {
 //        int number1 = this.toInt().intValue();
 //        int number2 = 0;
 //        int myltyply = 1;
@@ -73,28 +74,29 @@ public class ArrayInteger {
         byte memo = 0;
         byte remind = 0;
         for (int i = 0; i < num.digits.length; i++) {
-            if (i==this.digits.length-1) {
+            if (i == this.digits.length - 1) {
                 System.out.println("стоп");
             }
-            remind = (byte)((this.digits[i]+num.digits[i]) % 10 + memo);
-            System.out.print(i+" "+remind);
-            if (this.digits[i]+num.digits[i] >= 10) {
-                memo=(byte)((this.digits[i]+num.digits[i])/10);
+            remind = (byte) ((this.digits[i] + num.digits[i]) % 10 + memo);
+            System.out.print(i + " " + remind);
+            if (this.digits[i] + num.digits[i] >= 10) {
+                memo = (byte) ((this.digits[i] + num.digits[i]) / 10);
             } else {
                 memo = 0;
             }
             this.digits[i] = remind;
             System.out.println(" остаток " + memo);
-            if (i == num.digits.length-1 & num.digits.length < this.digits.length & memo != 0){
-                this.digits[i+1] = (byte) (this.digits[i+1] + memo);
+            if (i == num.digits.length - 1 & num.digits.length < this.digits.length & memo != 0) {
+                this.digits[i + 1] = (byte) (this.digits[i + 1] + memo);
+                System.out.println("размер второго числа меньше первого и есть разряд для записи в следующий элемент массива");
             }
 
-            if (i == this.digits.length-1){
-                int lastNum=0;
-                for (int j=i+1; j<num.digits.length; j++){
-                    lastNum+=num.digits[j];
+            if (i == this.digits.length - 1) {
+                int lastNum = 0;
+                for (int j = i + 1; j < num.digits.length; j++) {
+                    lastNum += num.digits[j];
                 }
-                if (memo != 0 | lastNum !=0){
+                if (memo != 0 | lastNum != 0) {
                     Arrays.fill(digits, (byte) 0);
                     return false;
                 } else {
@@ -108,12 +110,12 @@ public class ArrayInteger {
 
     public static void main(String[] args) {
         ArrayInteger arrI1 = new ArrayInteger(8);
-        arrI1.fromInt(new BigInteger("12099845"));
+        arrI1.fromInt(new BigInteger("12799845"));
         System.out.println(Arrays.toString(arrI1.digits));
         System.out.println(arrI1.toInt());
 
-        ArrayInteger arrI2 = new ArrayInteger(25);
-        arrI2.fromInt(new BigInteger("92099845"));
+        ArrayInteger arrI2 = new ArrayInteger(6);
+        arrI2.fromInt(new BigInteger("920998"));
         System.out.println(arrI1.add(arrI2));
         System.out.println(Arrays.toString(arrI1.digits));
 
