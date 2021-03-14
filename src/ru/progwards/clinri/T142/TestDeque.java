74de229413389a2f423dfcd8db218133958041a1
clinri <and87ep@gmail.com>
2021-03-14 23:53:43 +0300

    HomeWorks H15, Задача 2 - доделка из дома

--- src/ru/progwards/clinri/T142/TestDeque.java
+++ src/ru/progwards/clinri/T142/TestDeque.java
@@ -1,6 +1,7 @@
 package ru.progwards.clinri.T142;
 
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 
 //Реализуйте метод с сигнатурой int sumLastAndFirst(ArrayDeque<Integer> deque)
 // который возвращает сумму первого и последнего элемента очереди. При пустой очереди вернуть 0
@@ -8,5 +9,31 @@ public class TestDeque {
     int sumLastAndFirst(ArrayDeque<Integer> deque){
         if (deque.isEmpty()) return 0;
         return deque.peekFirst()+deque.peekLast();
+    }
+
+    static void dequeueTest() {
+        ArrayDeque deque = new ArrayDeque<>();
+
+        for (int i = 0; i <= 10; i++) {
+            deque.offer(i);
+            if (i%2 == 0)
+                deque.poll();
+        }
+
+        System.out.println(deque);
+    }
+
+    static void pqTest() {
+        PriorityQueue pQueue = new PriorityQueue<>();
+        pQueue.offer(10);
+        pQueue.offer(1);
+        pQueue.offer(9);
+        pQueue.offer(3);
+        System.out.println(pQueue);
+    }
+
+    public static void main(String[] args) {
+        //dequeueTest();
+        pqTest();
     }
 }
